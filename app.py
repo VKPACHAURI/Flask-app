36290433f4552183e212de95ba212ca56eeb6de9
vishesh_pachauri <visheshpachauri957@gmail.com>
2024-09-14 03:02:35 +0530

    flask-commit

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+from flask.app import Flask
 from flask import Flask
 
 
 app=Flask(__name__)
 
 @app.route("/") #route is decorator
-def greet():
+def name():
     return "Flask app is running!!!!!"
 
