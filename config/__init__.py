518e4b6b7c0bb78bb3ed6e631ebd4c325904b0a0
vishesh_pachauri <visheshpachauri957@gmail.com>
2024-09-14 21:27:09 +0530

    flask-commit

--- config/__init__.py
+++ config/__init__.py
@@ -1,3 +1,5 @@
 from flask import Flask
-
+import os
 app=Flask(__name__,static_url_path="")
+app.config['SQLALCHEMY_DATABASE_URI']=os.geienv("MYSQL_URL_PATTERN")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
